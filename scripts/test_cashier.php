c5f21ae8e7249a2190dbb1bf7460ae209fd8828f
Joshua <delaralloydjoshua4@gmail.com>
2026-04-29 16:00:21 +0800

    Fix broken require paths in scripts/ folder (install, migrate, test files)

--- scripts/test_cashier.php
+++ scripts/test_cashier.php
@@ -1,5 +1,5 @@
 <?php
 $_SESSION['user'] = ['id' => 2, 'username' => 'cashier_test', 'role' => 'cashier'];
 ob_start();
-require 'kubo.php';
-file_put_contents('test_cashier_output.txt', ob_get_clean());
+require __DIR__ . '/../kubo.php';
+file_put_contents(__DIR__ . '/test_cashier_output.txt', ob_get_clean());
